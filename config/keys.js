1eefca52ffd48c17471ee731774b76a3ef77f738
Gavin Quirk <gavinquirk@gmail.com>
2019-02-15 13:59:44 -0800

    server setup and mlab connection

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://gavin:password1@ds115583.mlab.com:15583/devconnector'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initialize
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(error => console.log(error));
+
+// Home Route
+app.get('/', (req, res) => res.send('Hello'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
