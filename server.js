2239e2d518f4d21a43bb4a63a231ad5707015336
Gavin Quirk <gavinquirk@gmail.com>
2019-02-15 14:21:38 -0800

    api routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// API Routes
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
 // Initialize
 const app = express();
 
@@ -16,6 +21,12 @@ mongoose
 // Home Route
 app.get('/', (req, res) => res.send('Hello'));
 
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// PORT - Heroku or localhost:5000
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
