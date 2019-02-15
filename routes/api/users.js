0a9e2e496730a4af5b48ddfb19d13b378d05cbcc
Gavin Quirk <gavinquirk@gmail.com>
2019-02-15 15:19:32 -0800

    route and logic for login -- still needs to provide token

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,7 +20,7 @@ router.post('/register', (req, res) => {
       if (user) {
         return res.status(400).json({ email: 'Email already exists' });
       } else {
-
+        // Grab gravatar url from user email
         const avatar = gravatar.url(req.body.email, {
           s: '200', // Size
           r: 'pg', // Rating
@@ -47,5 +47,32 @@ router.post('/register', (req, res) => {
       }
     })
 })
+
+// @route   GET api/users/login
+// @desc    Login User / Return JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email })
+    .then(user => {
+      //Check for user
+      if (!user) {
+        return res.status(404).json({ email: 'User not found' });
+      }
+
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' })
+          } else {
+            return res.status(400).json({ password: 'Password is incorrect' })
+          }
+        })
+    });
+});
 
 module.exports = router;
