17029cf4ad5872e3e1cc09f0f44c0e35b8eaf5ae
Gavin Quirk <gavinquirk@gmail.com>
2019-02-18 13:55:55 -0800

    validation for profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,91 +1,109 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
 
 // Load Models
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
-
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: "Profiles Works"}));
+router.get("/test", (req, res) => res.json({ msg: "Profiles Works" }));
 
 // @route   GET api/profile
 // @desc    Get current users profile
 // @access  Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const errors = {};
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
 
-  Profile.findOne({ user: req.user.id }) // logged in user object provided by passport
-    .then(profile => {
-      if (!profile) {
-        errors.noprofile = 'There is no profile for this user'
-        return res.status(404).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(error => res.status(404).json(error));
-});
+    Profile.findOne({ user: req.user.id }) // logged in user object provided by passport
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(error => res.status(404).json(error));
+  }
+);
 
 // @route   POST api/profile
 // @desc    Create or edit user profile
 // @access  Private
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  // Get fields
-  const profileFields = {};
-  profileFields.user = req.user.id // From passport
-  if (req.body.handle) profileFields.handle = req.body.handle;
-  if (req.body.company) profileFields.company = req.body.company;
-  if (req.body.website) profileFields.website = req.body.website;
-  if (req.body.location) profileFields.location = req.body.location;
-  if (req.body.bio) profileFields.bio = req.body.bio;
-  if (req.body.status) profileFields.status = req.body.status;
-  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-  // Skills - split into an array
-  if (typeof req.body.skills !== 'undefined') {
-    profileFields.skills = req.body.skills.split(',');
-  }
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
 
-  // Social
-  profileFields.social = {}
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    };
 
-  if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-  if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-  if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-  if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id; // From passport
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - split into an array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
 
-  // Search for logged in user
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
+    // Social
+    profileFields.social = {};
+
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    // Search for logged in user
+    Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
         // If user already has profile, update
-        Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
-          .then(profile => res.json(profile))
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
       } else {
         // If no profile exists, create
 
         // Check if handle exists -- send error if true
         Profile.findOne({ handle: profileFields.handle }).then(profile => {
           if (profile) {
-            errors.handle = 'That handle already exists';
-            res.status(400).json(errors)
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
           }
 
           // Save profile
           new Profile(profileFields).save().then(profile => {
             res.json(profile);
-          })
-        })
+          });
+        });
       }
-    })
-});
-
-
-
-
+    });
+  }
+);
 
 module.exports = router;
