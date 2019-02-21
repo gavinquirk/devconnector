f12e731ef77d6e2b0ea2064b8a5ac0e7eed1802d
Gavin Quirk <gavinquirk@gmail.com>
2019-02-20 18:58:43 -0800

    TextFieldGroup component

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { PropTypes }  from 'prop-types'
-import { connect } from 'react-redux'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Landing extends Component {
-
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard')
+      this.props.history.push('/dashboard');
     }
   }
 
@@ -44,10 +43,10 @@ class Landing extends Component {
 
 Landing.propTypes = {
   auth: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
 export default connect(mapStateToProps)(Landing);
