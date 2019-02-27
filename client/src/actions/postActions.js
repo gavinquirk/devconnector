023f723dcac4672cfa30a8da87a78a18b34b03fa
Gavin Quirk <gavinquirk@gmail.com>
2019-02-26 19:35:20 -0800

    deletePost, addLike, removeLike

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST
+} from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -21,8 +27,8 @@ export const addPost = postData => dispatch => {
 };
 
 // Get Posts
-export const getPosts = () => dispatch => {
-  dispatch(setPostLoading());
+export const getPosts = (load = true) => dispatch => {
+  if (load) dispatch(setPostLoading);
   axios
     .get('/api/posts')
     .then(response =>
@@ -35,6 +41,50 @@ export const getPosts = () => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: null
+      })
+    );
+};
+
+// Delete Post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(response =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(response => dispatch(getPosts()))
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(response => dispatch(getPosts()))
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
       })
     );
 };
--- client/src/components/Posts/PostItem.js
+++ client/src/components/Posts/PostItem.js
@@ -3,11 +3,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
+import { deletePost, addLike, removeLike } from '../../actions/postActions';
 
 class PostItem extends Component {
   onDeleteClick = id => {
-    console.log(id);
+    this.props.deletePost(id);
   };
+
+  onLikeClick = id => {
+    this.props.addLike(id);
+  };
+
+  onUnlikeClick = id => {
+    this.props.removeLike(id);
+  };
+
+  findUserLike(likes) {
+    const { auth } = this.props;
+    if (likes.filter(like => like.user === auth.user.id).length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   render() {
     const { post, auth } = this.props;
@@ -29,11 +47,19 @@ class PostItem extends Component {
           <div className='col-md-10'>
             <p className='lead'>{post.text}</p>
             <button type='button' className='btn btn-light mr-1'>
-              <i className='text-info fas fa-thumbs-up' />
+              <i
+                onClick={() => this.onLikeClick(post._id)}
+                className={classnames('fas fa-thumbs-up', {
+                  'text-info': this.findUserLike(post.likes)
+                })}
+              />
               <span className='badge badge-light'>{post.likes.length}</span>
             </button>
             <button type='button' className='btn btn-light mr-1'>
-              <i className='text-secondary fas fa-thumbs-down' />
+              <i
+                onClick={() => this.onUnlikeClick(post._id)}
+                className='text-secondary fas fa-thumbs-down'
+              />
             </button>
             <Link to={`/post/${post._id}`} className='btn btn-info mr-1'>
               Comments
@@ -56,11 +82,17 @@ class PostItem extends Component {
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps)(PostItem);
+export default connect(
+  mapStateToProps,
+  { deletePost, addLike, removeLike }
+)(PostItem);
