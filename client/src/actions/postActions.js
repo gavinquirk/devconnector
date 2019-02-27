80fb2d262c75c8d22d348389bdfedc3c3f8c1d2a
Gavin Quirk <gavinquirk@gmail.com>
2019-02-26 17:45:46 -0800

    reducer and action files, action types, addPost action for posts

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { ADD_POST, GET_ERRORS } from './types';
+
+// Add Post
+export const addPost = postData => dispatch => {
+  axios
+    .post('/api/posts', postData)
+    .then(response =>
+      dispatch({
+        type: ADD_POST,
+        payload: response.data
+      })
+    )
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    );
+};
